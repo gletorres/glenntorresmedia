ece58a50d909145d01975d3c3c31c154ee1b4a92
gletorres <torresglenn45@yahoo.com>
2022-06-15 00:38:20 +0800

    changing images

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./testimonials.css";
-import AVTR1 from "../../assets/avatar1.jpg";
-import AVTR2 from "../../assets/avatar2.jpg";
-import AVTR3 from "../../assets/avatar3.jpg";
-import AVTR4 from "../../assets/avatar4.jpg";
+import AVTR1 from "../../assets/avatar1.png";
+import AVTR2 from "../../assets/avatar2.png";
+import AVTR3 from "../../assets/avatar3.png";
+import AVTR4 from "../../assets/avatar4.png";
 
 // import Swiper core and required modules
 import { Pagination } from "swiper";
